d5183e2fe3d4ba75cae6004a9f4a455f8d1f4700
Nivedha G <109720688+Nivedha-govindan@users.noreply.github.com>
2022-10-15 15:45:03 +0530

    Updated validation.ps1

--- steps/01_registering-and-scanning-adls-gen2-account/checks/01_registered-and-scanned-adls-gen2-account/validation.ps1
+++ steps/01_registering-and-scanning-adls-gen2-account/checks/01_registered-and-scanned-adls-gen2-account/validation.ps1
@@ -1,21 +1,6 @@
 $WorkSpace = Get-AzResource -ResourceGroupName az-purview-labs -Resourcetype Microsoft.Purview/accounts
 $WorkSpacename = $WorkSpace.Name
 $TakeEndpoint = 'https://'+$WorkSpacename+'.purview.azure.com'
-$DataSource1 = Get-AzPurviewDataSource -Endpoint $TakeEndpoint -Name 'Sql-db'
-if($DataSource1){
-    Write-Host "SQL Database is Registered"
-}
-else{
-    Write-Host "SQL Database is not Registered"
-}
-$SourceSqlDatabasescan = Get-AzPurviewScan -Endpoint $TakeEndpoint -DataSourceName 'Sql-db' -Name 'Scan-db'
-if($SourceSqlServerscan){
-    Write-Host "SQL Database Scan is succeeded"
-}
-else{
-    Write-Host "SQL Database Scan is not succeeded"
-}
-
 
 $DataSource2 = Get-AzPurviewDataSource -Endpoint $TakeEndpoint -Name 'Adls-Gen2'
 if($DataSource2){
