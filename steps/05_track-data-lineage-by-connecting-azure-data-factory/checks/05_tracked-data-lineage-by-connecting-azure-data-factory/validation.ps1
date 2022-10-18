86808367a11301652f17477980c64f497d61adb1
Sameer Kulkarni <110413065+ksameer18@users.noreply.github.com>
2022-10-18 18:23:59 +0530

    Update validation.ps1

--- steps/05_track-data-lineage-by-connecting-azure-data-factory/checks/05_tracked-data-lineage-by-connecting-azure-data-factory/validation.ps1
+++ steps/05_track-data-lineage-by-connecting-azure-data-factory/checks/05_tracked-data-lineage-by-connecting-azure-data-factory/validation.ps1
@@ -5,26 +5,40 @@ param(
 )
 $WorkSpace = Get-AzResource -ResourceGroupName $Resourcegroupname -Resourcetype Microsoft.DataFactory/factories
 $WorkSpacename = $WorkSpace.Name
-$token = (Get-AzAccessToken -Resource "https://management.azure.com").Token
-$headers = @{ Authorization = "Bearer $token" }
 
-$uri = "https://management.azure.com/subscriptions/"+$Subscriptionid+"/resourceGroups/"+$Resourcegroupname+"/providers/Microsoft.DataFactory/factories/"+$WorkSpacename+"/pipelines/?api-version=2018-06-01"
-$res =Invoke-RestMethod -Method Get -ContentType "application/json" -Uri $uri -Headers $headers
+$token = (Get-AzAccessToken -Resource "https://purview.azure.net").Token
+$headers = @{ Authorization = "Bearer $token" }
+$body = "{
+  keywords: null,  
+  filter: {
+    or: [
+      {
+        assetType: ""Azure Data Factory""
+      }
+    ]
+  }
+}"
+$uri = "https://"+$WorkSpacename+".purview.azure.com/catalog/api/search/query?api-version=2022-03-01-preview&$select=id"
+$res = Invoke-RestMethod -Method Post -ContentType "application/json" -Uri $uri -Headers $headers -Body $body
 
 foreach($i in $res.value)
 {
-    foreach($j in $i){
-
-        if($j.name -like "CopyPipeline_as6")
+    foreach($j in $i)
+    {
+        if($j.name -like "Copy_as6")
         {
-            $pipeline = $j.name
+            $name = $j.name
         }
+        
     }
 }
 
-if($pipeline){
-    Write-Host "Pipeline created successfully"
+if($name)
+{
+    Write-Host "Pipeline activity ran successfully"
 }
-else{
-    Write-Host "Pipeline not created"
+else
+{
+    Write-Host "Pipeline activity doesn't exist"
 }
+
